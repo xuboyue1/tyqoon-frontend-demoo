e929a95e49e1f3a39392da80051a9a5d94f98c8f
wuhao <200927018@qq.com>
2024-09-03 17:04:32 +0800

    订阅

--- public/config.js
+++ public/config.js
@@ -1,5 +1,6 @@
-const base_url = "http://47.57.236.213:11111"
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzaG9wIiwiZXhwIjoxNzE2MjYyODIxLCJpYXQiOjE3MDg0ODY4MjEsImp0aSI6IjE5MDQwZWE1Y2I0MDg2YTg1NzIxNTJjNmZlMzIwOThjIn0.FjHvv0VBRjjbnJq-ekx4SWCbVgqpPu6nnsoY823bLSY"
+const base_url = "http://47.57.236.213:41111"
+const token = "\n" +
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzaG9wIiwiZXhwIjoxNzMzMDI0MDQ1LCJpYXQiOjE3MjUyNDgwNDUsImp0aSI6IjFkOWU3MzI3MGQyMjRlY2JhZWIxNDcyYzk5NzlhZjdjIn0.fNhg3j_w1T4mFsLDdQAIIrZci3oEoboh8hsJz4TZ9TU"
 
 
 async function getPlatform() {
@@ -29,6 +30,25 @@ async function getBalanceOrder(amount) {
     return {
         "businessType": 2,
         "businessId": data.id
+    }
+}
+
+async function buyVip() {
+    const req = {
+        "vipConfigId": "3",
+        "currency": "CNY",
+        "num": 1
+    }
+    const response = await fetch(`${base_url}` + "/user/order/buyVip", {
+        method: "POST",
+        headers: {"Content-Type": "application/json", "token": token},
+        body: JSON.stringify(req),
+    });
+    const {code, data} = await response.json();
+    console.log("business: ", data)
+    return {
+        "businessType": 5,
+        "businessId": data
     }
 }
 
---
+++ public/subscription/stripe/payment.js
@@ -0,0 +1,107 @@
+let stripe;
+let elements;
+let return_url;
+document.querySelector("#payment-form").addEventListener("submit", handleSubmit);
+
+
+const platformGroup = "STRIPE"
+
+const platformCode = [
+  "STRIPE",   // All payment methods included
+  "STRIPE-CARD",  // Card payment
+  "STRIPE-WECHAT_PAY", // Wechat Pay
+  "STRIPE-ALIPAY"  // Alipay
+]
+
+initialize()
+
+async function initialize() {
+  const platformList = await getPlatform()
+  for (const platform of platformList) {
+    if (platformGroup == platform.platformGroup) {
+      const elem= document.getElementById(platform.platformCode)
+      if (elem!=null){
+        elem.removeAttribute("hidden")
+      }
+
+    }
+  }
+}
+
+
+
+async function pay(code) {
+  setLoading(true);
+  document.getElementById("payment-select").setAttribute("hidden", true)
+  document.getElementById("payment-form").removeAttribute("hidden")
+  const order =await buyVip()
+  const res = await getPaymentOrder(code,order)
+
+  const clientJson = res.clientJson
+  const { clientSecret, pubKey,successUri } = clientJson
+  stripe = Stripe(pubKey);
+  return_url = successUri
+  const appearance = {
+    theme: 'stripe',
+  };
+  elements = stripe.elements({ appearance, clientSecret });
+  const paymentElement = elements.create("payment");
+  paymentElement.mount("#payment-element");
+
+  setLoading(false);
+}
+
+async function handleSubmit(e) {
+  e.preventDefault();
+  setLoading(true);
+
+  const { error } = await stripe.confirmSetup({
+    elements,
+    confirmParams: {
+      // Make sure to change this to your payment completion page
+      return_url:return_url,
+    },
+  });
+
+  // This point will only be reached if there is an immediate error when
+  // confirming the payment. Otherwise, your customer will be redirected to
+  // your `return_url`. For some payment methods like iDEAL, your customer will
+  // be redirected to an intermediate site first to authorize the payment, then
+  // redirected to the `return_url`.
+  if (error.type === "card_error" || error.type === "validation_error") {
+    showMessage(error.message);
+  } else {
+    showMessage("An unexpected error occurred.");
+  }
+
+  setLoading(false);
+}
+
+
+// ------- UI helpers -------
+
+function showMessage(messageText) {
+  const messageContainer = document.querySelector("#payment-message");
+
+  messageContainer.classList.remove("hidden");
+  messageContainer.textContent = messageText;
+
+  setTimeout(function () {
+    messageContainer.classList.add("hidden");
+    messageText.textContent = "";
+  }, 4000);
+}
+
+// Show a spinner on payment submission
+function setLoading(isLoading) {
+  if (isLoading) {
+    // Disable the button and show a spinner
+    document.querySelector("#submit").disabled = true;
+    document.querySelector("#spinner").classList.remove("hidden");
+    document.querySelector("#button-text").classList.add("hidden");
+  } else {
+    document.querySelector("#submit").disabled = false;
+    document.querySelector("#spinner").classList.add("hidden");
+    document.querySelector("#button-text").classList.remove("hidden");
+  }
+}
